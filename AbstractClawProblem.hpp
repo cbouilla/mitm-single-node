7febdd85723bf05d9370e0f0826074df7330b57e
Ahmed K. Alharbi <48549687+akaalharbi@users.noreply.github.com>
2024-01-31 16:04:42 +0100

    only indentaion

--- AbstractClawProblem.hpp
+++ AbstractClawProblem.hpp
@@ -6,58 +6,58 @@
 #include "include/prng.hpp"
 
 namespace mitm {
-/*
- * Provides the description of the types A, B and C, as well as 
- * two functions f : A -> C and g : B -> C and an optionnal
- * predicate P.
- *
- * The problem instance can contain extra data.
- * E.g. In the attack on double-encryption, where the goal is to find
- *      x, y s.t. f(x, a) == g(y, b), the problem should contain (a, b).
- */
+  /*
+   * Provides the description of the types A, B and C, as well as 
+   * two functions f : A -> C and g : B -> C and an optionnal
+   * predicate P.
+   *
+   * The problem instance can contain extra data.
+   * E.g. In the attack on double-encryption, where the goal is to find
+   *      x, y s.t. f(x, a) == g(y, b), the problem should contain (a, b).
+   */
 template<typename I, typename A, typename B, typename Domain_C>
 class AbstractClawProblem {
 public:
-        /* these lines have to be retyped again */
-        using C_t = typename Domain_C::t;
+  /* these lines have to be retyped again */
+  using C_t = typename Domain_C::t;
   
-        AbstractClawProblem() {
-                static_assert(std::is_base_of<AbstractDomain<typename Domain_C::t>, Domain_C>::value,
-                  "C not derived from AbstractDomain");
-                // C needs to have hash_1_bit
-        }
+  AbstractClawProblem() {
+    static_assert(std::is_base_of<AbstractDomain<typename Domain_C::t>, Domain_C>::value,
+		  "C not derived from AbstractDomain");
+    // C needs to have hash_1_bit
+  }
   
-        /* specification of the collision to find */
+  /* specification of the collision to find */
 
-        void f(const A &x, C_t &y) const;  /* y <--- f(x) */
-        void g(const B &x, C_t &y) const;  /* y <--- g(x) */
+  void f(const A &x, C_t &y) const;  /* y <--- f(x) */
+  void g(const B &x, C_t &y) const;  /* y <--- g(x) */
         
-        /* assuming that f(x) == g(y) == z, is (x, y) an acceptable outcome? */
-        bool good_collision(const A &x, const B &y, const C_t &z) const 
-        { 
-                return true;    // by default, yes.
-        }
+  /* assuming that f(x) == g(y) == z, is (x, y) an acceptable outcome? */
+  bool good_collision(const A &x, const B &y, const C_t &z) const 
+  { 
+    return true;    // by default, yes.
+  }
 
-        /* embedding and randomization */
+  /* embedding and randomization */
 
-        /*
-         * TODO: we need to formalize what we expect from these two functions.
-         * It seems reasonable to ask that there must exist a subset D of C 
-         * such that these two, when their domain is restricted to D, are injective. 
-         */
-        void send_C_to_A(const C_t &inp_C, A &out_A) const;
-        void send_C_to_B(const C_t &inp_C, B &out_B) const;
+  /*
+   * TODO: we need to formalize what we expect from these two functions.
+   * It seems reasonable to ask that there must exist a subset D of C 
+   * such that these two, when their domain is restricted to D, are injective. 
+   */
+  void send_C_to_A(const C_t &inp_C, A &out_A) const;
+  void send_C_to_B(const C_t &inp_C, B &out_B) const;
 
-        /*
-         * Family of permutations acting on the output domain.
-         */
-        void mix(const I &i, const C_t& x, C_t& y) const;   /* y <--- f_i(x) */
+  /*
+   * Family of permutations acting on the output domain.
+   */
+  void mix(const I &i, const C_t& x, C_t& y) const;   /* y <--- f_i(x) */
 
-        /* Generate a default permutation of C (e.g. the identity) */
-        I& mix_default() const; 
+  /* Generate a default permutation of C (e.g. the identity) */
+  I& mix_default() const; 
 
-        /* Generate a new random permutation of C */
-        I& mix_sample(PRNG& rng) const; 
+  /* Generate a new random permutation of C */
+  I& mix_sample(PRNG& rng) const; 
 };
 
 }
--- AbstractCollisionProblem.hpp
+++ AbstractCollisionProblem.hpp
@@ -16,52 +16,52 @@ namespace mitm {
 template<typename I, typename A, typename Domain_C>
 class AbstractCollisionProblem {
 public:
-	/* these lines have to be retyped again */
-	using C_t = typename Domain_C::t;
+  /* these lines have to be retyped again */
+  using C_t = typename Domain_C::t;
   
-	AbstractCollisionProblem() {
-		// enforce that C is a subclass of AbstractDomain
-		// In fact, we don't need to know **anything** about A
-		static_assert(std::is_base_of<AbstractDomain<typename Domain_C::t>, Domain_C>::value,
-	  		"C not derived from AbstractDomain");
-	                // C does not need to have hash_1_bit
-	}
+  AbstractCollisionProblem() {
+    // enforce that C is a subclass of AbstractDomain
+    // In fact, we don't need to know **anything** about A
+    static_assert(std::is_base_of<AbstractDomain<typename Domain_C::t>, Domain_C>::value,
+		  "C not derived from AbstractDomain");
+    // C does not need to have hash_1_bit
+  }
   
-	/* 
-	 * problem: to invoke the "is_equal(x, y)" method from the domain, we need an
-	 * object of type Domain_C, and we don't have any...
-	 */
+  /* 
+   * problem: to invoke the "is_equal(x, y)" method from the domain, we need an
+   * object of type Domain_C, and we don't have any...
+   */
 
-	/* specification of the collision to find */
+  /* specification of the collision to find */
 
 
-	void f(const A &x, C_t &y) const;  /* y <--- f(x) */
+  void f(const A &x, C_t &y) const;  /* y <--- f(x) */
   
-	/* assuming that f(x) == f(y) == z and x != y, is (x, y) an acceptable outcome? */
-	bool good_collision(const A_t &x, const A_t &y, const C_t &z) const 
-	{ 
-	 	return true;  // by default, yes. 
-	}
+  /* assuming that f(x) == f(y) == z and x != y, is (x, y) an acceptable outcome? */
+  bool good_collision(const A_t &x, const A_t &y, const C_t &z) const 
+  { 
+    return true;  // by default, yes. 
+  }
 	
-	/* embedding and randomization */
+  /* embedding and randomization */
 
-	/*
-	 * TODO: we need to formalize what we expect from this function.
-	 * It seems reasonable to ask that there must exist a subset D of C 
-	 * such that the embedding, when its domain is restricted to D, is injective. 
-	 */
-	void send_C_to_A(const C_t &inp_C, A& out_A) const;
+  /*
+   * TODO: we need to formalize what we expect from this function.
+   * It seems reasonable to ask that there must exist a subset D of C 
+   * such that the embedding, when its domain is restricted to D, is injective. 
+   */
+  void send_C_to_A(const C_t &inp_C, A& out_A) const;
 	
-	/*
-	 * Family of permutations acting on the output domain.
-	 */
-	void mix(const I &i, const C_t& x, C_t& y) const;   /* y <--- f_i(x) */
+  /*
+   * Family of permutations acting on the output domain.
+   */
+  void mix(const I &i, const C_t& x, C_t& y) const;   /* y <--- f_i(x) */
 
-	/* Generate a default permutation of C (e.g. the identity) */
-	I& mix_default() const; 
+  /* Generate a default permutation of C (e.g. the identity) */
+  I& mix_default() const; 
 
-	/* Generate a new random permutation of C */
-	I& mix_sample(PRNG& rng) const; 
+  /* Generate a new random permutation of C */
+  I& mix_sample(PRNG& rng) const; 
 };
 }
 
