0d2146bbccb7048ad47c12e915d09cc1489a59a4
Ahmed K. Alharbi <48549687+akaalharbi@users.noreply.github.com>
2024-02-09 17:55:17 +0100

    base (empty) file for naive algorithm to verify if all collisions were found

---
+++ naive_engine.hpp
@@ -0,0 +1,14 @@
+#ifndef NAIVE_ENGINE
+#define NAIVE_ENGINE
+#include <vector>
+/* Generate all collisions/claws using naive algorithm. */
+
+namespace mitm
+{
+template<typename Problem, typename X_t, typename Y_t>
+
+}    
+
+
+
+#endif
