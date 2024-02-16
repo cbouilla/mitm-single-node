1fede82f765e5346053deae7d0274af2e7488367
Ahmed K. Alharbi <48549687+akaalharbi@users.noreply.github.com>
2024-02-16 17:24:20 +0100

    naive functionalities seem to be complete.

--- naive_engine.hpp
+++ naive_engine.hpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <tuple>
 #include <algorithm>
+#include <execution>
+#include <omp.h>
 #include "AbstractDomain.hpp"
 
 /* Generate all collisions/claws using naive algorithm. */
@@ -19,6 +21,7 @@ template<typename X_t, typename Y_t>
 auto images(void (*f)(X_t const& x, Y_t& y), /* y <- f(x) */
 	    void (*ith_element)(X_t&, size_t),/* x = i */
 	    void (*copy)(Y_t& y1, Y_t& y2), /* y2 := y1 */
+	    bool (*cmp)(Y_t& y1, Y_t& y2),
 	    size_t start, /* index of first x to treat */
 	    size_t end /* index of last x to treat. it's can be generated*/
 	    ) -> std::vector<Y_t>
@@ -53,6 +56,13 @@ auto images(void (*f)(X_t const& x, Y_t& y), /* y <- f(x) */
     }
   }
 
+  /* sort outputs in parallel */
+  std::sort(std::execution::par,
+	    outputs.begin(),
+	    outputs.end(),
+	    cmp);
+  
+
   return outputs;
 }
 
@@ -82,14 +92,13 @@ auto collisions(void (*copy)(const Y_t &y1, Y_t &y2),     /* y2 := y1 */
 {
   std::vector<Y_t> collisions{};
   size_t n_elements_f = f_images.size();
-  size_t n_elements_g = g_images.size();  
+  size_t n_found_collisions = 0;
   
-
-
    /* Parallelization could start from here */
   size_t nthreads = 1;
   for (size_t thd=0; thd < nthreads; ++thd){
     std::vector<Y_t> collisions_thd{};
+    int result = 0;
 
     /* which indices of f to read: */
     size_t idx_start_f = thd*(n_elements_f/nthreads);
@@ -97,28 +106,65 @@ auto collisions(void (*copy)(const Y_t &y1, Y_t &y2),     /* y2 := y1 */
     /* what indices of g to read */
     // constexpr ForwardIt lower_bound( ForwardIt first, ForwardIt last,
     //                                  const T& value, Compare comp );
-    auto start_g = std::lower_bound(g_images.begin(), g_images.end(), cmp_with_start_f);
-    auto end_g = std::upper_bound(g_images.begin(), g_images.end(), cmp_with_end_f);
+    auto start_g = std::lower_bound(g_images.begin(),
+				    g_images.end(),
+				    f_images[idx_start_f],
+				    [cmp](Y_t& a, Y_t& b){return (cmp(a, b) == -1);}); // cmp_with_start_f);
+
+    auto end_g = std::upper_bound(g_images.begin(),
+				  g_images.end(),
+				  [cmp](Y_t& a, Y_t& b){return (cmp(a, b) < 0);});
 
     auto start_f = (f_images.begin() + idx_start_f);
     auto end_f = (f_images.begin() + idx_end_f);
 
     while ( (start_g != end_g) || (start_f != end_f) ) {
       /* if they are equal, store the collision */
+      result = cmp(*start_f, *start_g);
+      if (result == 0){
+	collisions_thd.push_back(*start_f);
+	++start_f;
+	++start_g;
+      }
 
       /* elif f < g, forward only f */
+      if (result < 0)
+	++start_f;
 
       /* elif g < f, forward only g */
+      if (result > 0)
+	++start_g;
     }
-    
-  }
-  
-  
 
+    #pragma omp atomic
+    { n_found_collisions += collisions_thd.size(); }
+    if (thd == 0) /* thd0 is always responsible for the grand list of collisions */
+      collisions.reserve(n_found_collisions);
+    /* merge vectors  */
+    #pragma omp critical
+    {
+      collisions.insert(collisions.begin(), collisions_thd.begin(), collisions_thd.end());
+    }
+  } /* end of threads work */
+  /* sort collisions in parallel */
+  std::sort(std::execution::par,
+	    collisions.begin(),
+	    collisions.end(),
+	    [cmp](Y_t const& a, Y_t const& b){return (cmp(a, b) < 0);}
+	    );
 
-
+  return collisions;
 }
 
 // merge vectors with master
 
 #endif
+
+
+
+// /* sort the input output according to the second element (the output ) */
+// std::sort(std::execution::par,
+// 	    inp_out.begin(),
+// 	    inp_out.end(),
+// 	    [](std::pair<A_t, C_serial> io1, std::pair<A_t, C_serial> io2)
+// 	    {return io1.second < io2.second; });
