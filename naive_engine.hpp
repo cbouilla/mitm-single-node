e9a8dbd682e39423dc6ab450cee0bd98deaaf9f8
Ahmed K. Alharbi <48549687+akaalharbi@users.noreply.github.com>
2024-02-18 19:10:01 +0100

    fixed the naive_collision issue, it remains to put the pieces together and write a comparision function

--- naive_engine.hpp
+++ naive_engine.hpp
@@ -10,6 +10,39 @@
 #include <omp.h>
 #include "AbstractDomain.hpp"
 
+
+/* return -1 if a < b, 0 if a == b, 1 if a > b */
+template <typename Domain>
+int compare(const typename Domain::t& a,
+	    const typename Domain::t& b)
+{
+  Domain dom{};
+  static uint8_t a_serial[dom.length];
+  static uint8_t b_serial[dom.length];
+
+  dom.serialize(a, a_serial);
+  dom.serialize(b, b_serial);
+
+  return std::memcmp(a_serial, b_serial, dom.length);
+}
+
+
+/* given (a0, a1) and (b0, b1) return -1 if a0 < b0, 0 if a0 == b0, 1 if a0 > b0 */
+template <typename Domain, typename Y_t, typename X_t>
+int compare_pair(const typename std::pair<Y_t, X_t>& a,
+		 const typename std::pair<Y_t, X_t>& b)
+{/* todo take a1, b1 into considerations */
+  Domain dom{};
+  static uint8_t a_serial[dom.length];
+  static uint8_t b_serial[dom.length];
+
+  dom.serialize(a.first, a_serial);
+  dom.serialize(b.first, b_serial);
+
+  return std::memcmp(a_serial, b_serial, dom.length);
+}
+
+
 /* Generate all collisions/claws using naive algorithm. */
 
 
@@ -20,7 +53,6 @@
 template<typename X_t, typename Y_t>
 auto images(void (*f)(X_t const& x, Y_t& y), /* y <- f(x) */
 	    void (*ith_element)(X_t&, size_t),/* x = i */
-	    void (*copy)(Y_t& y1, Y_t& y2), /* y2 := y1 */
 	    bool (*cmp)(Y_t& y1, Y_t& y2),
 	    size_t start, /* index of first x to treat */
 	    size_t end /* index of last x to treat. it's can be generated*/
@@ -51,7 +83,7 @@ auto images(void (*f)(X_t const& x, Y_t& y), /* y <- f(x) */
     for (size_t i = 0; i < nelement_per_thd; ++i){
       f(x, y);
       /* todo make it by index */
-      copy(y, outputs[ i + offset ]);
+      outputs[ i + offset ] = y;
       ith_element(x, i + global_offset );
     }
   }
@@ -66,38 +98,74 @@ auto images(void (*f)(X_t const& x, Y_t& y), /* y <- f(x) */
   return outputs;
 }
 
-/* return -1 if a < b, 0 if a == b, 1 if a > b */
-Ootemplate <typename Domain>
-int compare(const typename Domain::t& a,
-	    const typename Domain::t& b)
+
+/* returns (X, f(X))*/
+template<typename X_t, typename Y_t>
+auto domain_images(void (*f)(X_t const& x, Y_t& y), /* y <- f(x) */
+		   void (*ith_element)(X_t&, size_t),/* x = i */
+		   bool (*cmp)(Y_t& y1, Y_t& y2),
+		   size_t start, /* index of first x to treat */
+		   size_t end /* index of last x to treat. it's can be generated*/
+		   ) -> std::vector< std::pair<X_t, Y_t> >
 {
-  Domain dom{};
-  static uint8_t a_serial[dom.length];
-  static uint8_t b_serial[dom.length];
+  size_t n_elements = end - start; /* to be processed in this function */
+  std::vector< std::pair<X_t, Y_t> > outputs{};
+  outputs.resize(n_elements);
 
-  dom.serialize(a, a_serial);
-  dom.serialize(b, b_serial);
 
-  return std::memcmp(a_serial, b_serial, dom.length);
+  /* Parallelization could start from here */
+  size_t nthreads = 1;
+  
+  for (size_t thd = 0; thd < nthreads; ++thd){
+    Y_t y{};
+    X_t x{};
+
+    /* thd j, will work on indices that starts with offset */
+    const size_t offset =  thd*(n_elements/nthreads);
+    const size_t global_offset = start + offset; /*  globally which x to treat */
+
+    /* get the ith input */
+    ith_element(x, 0 + global_offset);
+    /* last term accounts for r in  n_elements = nthreads*k + r, in the last thread */
+    size_t nelement_per_thd = (n_elements/nthreads) + (n_elements%n_elements)*(thd == (nthreads -1));
+
+    /* A thread main work */
+    for (size_t i = 0; i < nelement_per_thd; ++i){
+      f(x, y);
+      /* todo make it by index */
+      outputs[ i + offset ] = std::pair(y, x);
+      ith_element(x, i + global_offset );
+    }
+  }
+
+  /* sort outputs in parallel */
+  std::sort(std::execution::par,
+	    outputs.begin(),
+	    outputs.end(),
+	    cmp);
+  
+
+  return outputs;
 }
+
 
  
 /* return {y: there is u, v satisfies y = f(u) = g(v) } sorted by the image where f: X -> Y. */
-template <typename X_t, typename Y_t>
-auto collisions(void (*copy)(const Y_t &y1, Y_t &y2),     /* y2 := y1 */
-		int (*cmp)  (const Y_t& y1, const Y_t& y2),
-		const std::vector<Y_t>& f_images,
-		const std::vector<Y_t>& g_images		
-		) -> std::vector<Y_t>
+//template <typename X_t, typename Y_t>
+template <typename T>
+auto extract_collisions(int (*cmp)  (const T& y1, const T& y2),
+			const std::vector<T>& f_images,
+			const std::vector<T>& g_images		
+			) -> std::vector<T>
 {
-  std::vector<Y_t> collisions{};
+  std::vector<T> collisions{};
   size_t n_elements_f = f_images.size();
   size_t n_found_collisions = 0;
   
    /* Parallelization could start from here */
   size_t nthreads = 1;
   for (size_t thd=0; thd < nthreads; ++thd){
-    std::vector<Y_t> collisions_thd{};
+    std::vector<T> collisions_thd{};
     int result = 0;
 
     /* which indices of f to read: */
@@ -109,11 +177,11 @@ auto collisions(void (*copy)(const Y_t &y1, Y_t &y2),     /* y2 := y1 */
     auto start_g = std::lower_bound(g_images.begin(),
 				    g_images.end(),
 				    f_images[idx_start_f],
-				    [cmp](Y_t& a, Y_t& b){return (cmp(a, b) == -1);}); // cmp_with_start_f);
+				    [cmp](T& a, T& b){return (cmp(a, b) == -1);}); // cmp_with_start_f);
 
     auto end_g = std::upper_bound(g_images.begin(),
 				  g_images.end(),
-				  [cmp](Y_t& a, Y_t& b){return (cmp(a, b) < 0);});
+				  [cmp](T& a, T& b){return (cmp(a, b) < 0);});
 
     auto start_f = (f_images.begin() + idx_start_f);
     auto end_f = (f_images.begin() + idx_end_f);
@@ -143,8 +211,8 @@ auto collisions(void (*copy)(const Y_t &y1, Y_t &y2),     /* y2 := y1 */
     #pragma omp barrier /* all threads should've finished dealing with images_f, and images_g */
     if (thd == 0){
       /* free spaces used by images_g and images_f */
-      f_images = std::vector<Y_t>();
-      g_images = std::vector<Y_t>();
+      f_images = std::vector<T>();
+      g_images = std::vector<T>();
       collisions.reserve(n_found_collisions);
     } /* thd0 is always responsible for the grand list of collisions */
     #pragma omp barrier /* all threads should've finished dealing with images_f, and images_g */
@@ -153,16 +221,18 @@ auto collisions(void (*copy)(const Y_t &y1, Y_t &y2),     /* y2 := y1 */
       
     /* merge vectors  */
     
-   #pragma omp critical
-    {
-      collisions.insert(collisions.begin(), collisions_thd.begin(), collisions_thd.end());
+    #pragma omp critical
+    {/* add thd found collisions to the shared list */
+      collisions.insert(collisions.begin(),
+			collisions_thd.begin(),
+			collisions_thd.end());
     }
   } /* end of threads work */
   /* sort collisions in parallel */
   std::sort(std::execution::par,
 	    collisions.begin(),
 	    collisions.end(),
-	    [cmp](Y_t const& a, Y_t const& b){return (cmp(a, b) < 0);}
+	    [cmp](T const& a, T const& b){return (cmp(a, b) < 0);}
 	    );
 
   return collisions;
@@ -178,7 +248,7 @@ auto collisions(void (*copy)(const Y_t &y1, Y_t &y2),     /* y2 := y1 */
 template <typename Problem>
 void naive_collisoin_search(Problem& Pb)
 {
-  /* aïe aïe aïe, just realized that it won't work looking at the inputs! */
+
 }
 
 
