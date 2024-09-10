fd5330b9c45b2c733fae7b0da1b522d49a94b843
Charles Bouillaguet <charles.bouillaguet@lip6.fr>
2024-09-10 19:17:32 +0200

    stupid

--- examples/usuba_des.c
+++ examples/usuba_des.c
@@ -2,7 +2,79 @@
 
 #include "types.h"
 
-#if defined(__AVX2__)
+
+#if defined(__AVX512F__)
+#include <immintrin.h>
+
+#define ZERO _mm512_setzero_si512()
+#define ONES _mm512_set1_epi32(-1)
+
+#define AND(a,b)  _mm512_and_si512(a,b)
+#define OR(a,b)   _mm512_or_si512(a,b)
+#define XOR(a,b)  _mm512_xor_si512(a,b)
+#define ANDN(a,b) _mm512_andnot_si512(a,b)
+#define NOT(a)    _mm512_xor_si512(ONES,a)
+
+#define DATATYPE __m512i
+#define VLEN 512
+///////////////////////////////////////////////////////////////////////////////////
+
+static void real_ortho_512x512(__m512i data[]) {
+
+  __m512i mask_l[9] = {
+  _mm512_set1_epi64(0xaaaaaaaaaaaaaaaaUL),
+  _mm512_set1_epi64(0xccccccccccccccccUL),
+  _mm512_set1_epi64(0xf0f0f0f0f0f0f0f0UL),
+  _mm512_set1_epi64(0xff00ff00ff00ff00UL),
+  _mm512_set1_epi64(0xffff0000ffff0000UL),
+  _mm512_set1_epi64(0xffffffff00000000UL),
+  _mm512_set_epi64(0UL,-1UL,0UL,-1UL,0UL,-1UL,0UL,-1UL),
+  _mm512_set_epi64(0UL,0UL,-1UL,-1UL,0UL,0UL,-1UL,-1UL),
+  _mm512_set_epi64(0UL,0UL,0UL,0UL,-1UL,-1UL,-1UL,-1UL),
+  };
+
+  __m512i mask_r[9] = {
+  _mm512_set1_epi64(0x5555555555555555UL),
+  _mm512_set1_epi64(0x3333333333333333UL),
+  _mm512_set1_epi64(0x0f0f0f0f0f0f0f0fUL),
+  _mm512_set1_epi64(0x00ff00ff00ff00ffUL),
+  _mm512_set1_epi64(0x0000ffff0000ffffUL),
+  _mm512_set1_epi64(0x00000000ffffffffUL),
+  _mm512_set_epi64(-1UL,0UL,-1UL,0UL,-1UL,0UL,-1UL,0UL),
+  _mm512_set_epi64(-1UL,-1UL,0UL,0UL,-1UL,-1UL,0UL,0UL),
+  _mm512_set_epi64(-1UL,-1UL,-1UL,-1UL,0UL,0UL,0UL,0UL),
+  };
+  
+  for (int i = 0; i < 9; i ++) {
+  int n = (1UL << i);
+  for (int j = 0; j < 512; j += (2 * n))
+    for (int k = 0; k < n; k ++) {
+    __m512i u = _mm512_and_si512(data[j + k], mask_l[i]);
+    __m512i v = _mm512_and_si512(data[j + k], mask_r[i]);
+    __m512i x = _mm512_and_si512(data[j + n + k], mask_l[i]);
+    __m512i y = _mm512_and_si512(data[j + n + k], mask_r[i]);
+    if (i <= 5) {
+      data[j + k] = _mm512_or_si512(u, _mm512_srli_epi64(x, n));
+      data[j + n + k] = _mm512_or_si512(_mm512_slli_epi64(v, n), y);
+    } else if (i == 6) {
+      /* Note the "inversion" of srli and slli. */
+      data[j + k] = _mm512_or_si512(u, _mm512_bslli_epi128(x, 8));
+      data[j + n + k] = _mm512_or_si512(_mm512_bsrli_epi128(v, 8), y);
+    } else if (i == 7) {
+      /* might be 0b01001110 instead */
+      data[j + k] = _mm512_or_si512(u, _mm512_permutex_epi64(x,0b10110001));
+      data[j + n + k] = _mm512_or_si512(_mm512_permutex_epi64(v,0b10110001), y);
+    } else {
+      /* might be 0,1,2,3,4,5,6,7 */
+      __m512i ctrl = _mm512_set_epi64(4,5,6,7,0,1,2,3);
+      data[j + k] = _mm512_or_si512(u, _mm512_permutexvar_epi64(ctrl,x));
+      data[j + n + k] = _mm512_or_si512(_mm512_permutexvar_epi64(ctrl,v), y);
+    }
+    }
+  }
+}
+
+#elif defined(__AVX2__)
 #include <immintrin.h>
 
 #define ZERO _mm256_setzero_si256()
@@ -67,79 +139,10 @@ static void real_ortho_256x256(__m256i data[]) {
   }
 }
 
-#elif defined(__AVX512F__)
-#include <immintrin.h>
-
-#define ZERO _mm512_setzero_si512()
-#define ONES _mm512_set1_epi32(-1)
-
-#define AND(a,b)  _mm512_and_si512(a,b)
-#define OR(a,b)   _mm512_or_si512(a,b)
-#define XOR(a,b)  _mm512_xor_si512(a,b)
-#define ANDN(a,b) _mm512_andnot_si512(a,b)
-#define NOT(a)    _mm512_xor_si512(ONES,a)
-
-#define DATATYPE __m512i
-#define VLEN 512
-///////////////////////////////////////////////////////////////////////////////////
-
-static void real_ortho_512x512(__m512i data[]) {
-
-  __m512i mask_l[9] = {
-	_mm512_set1_epi64(0xaaaaaaaaaaaaaaaaUL),
-	_mm512_set1_epi64(0xccccccccccccccccUL),
-	_mm512_set1_epi64(0xf0f0f0f0f0f0f0f0UL),
-	_mm512_set1_epi64(0xff00ff00ff00ff00UL),
-	_mm512_set1_epi64(0xffff0000ffff0000UL),
-	_mm512_set1_epi64(0xffffffff00000000UL),
-	_mm512_set_epi64(0UL,-1UL,0UL,-1UL,0UL,-1UL,0UL,-1UL),
-	_mm512_set_epi64(0UL,0UL,-1UL,-1UL,0UL,0UL,-1UL,-1UL),
-	_mm512_set_epi64(0UL,0UL,0UL,0UL,-1UL,-1UL,-1UL,-1UL),
-  };
-
-  __m512i mask_r[9] = {
-	_mm512_set1_epi64(0x5555555555555555UL),
-	_mm512_set1_epi64(0x3333333333333333UL),
-	_mm512_set1_epi64(0x0f0f0f0f0f0f0f0fUL),
-	_mm512_set1_epi64(0x00ff00ff00ff00ffUL),
-	_mm512_set1_epi64(0x0000ffff0000ffffUL),
-	_mm512_set1_epi64(0x00000000ffffffffUL),
-	_mm512_set_epi64(-1UL,0UL,-1UL,0UL,-1UL,0UL,-1UL,0UL),
-	_mm512_set_epi64(-1UL,-1UL,0UL,0UL,-1UL,-1UL,0UL,0UL),
-	_mm512_set_epi64(-1UL,-1UL,-1UL,-1UL,0UL,0UL,0UL,0UL),
-  };
-  
-  for (int i = 0; i < 9; i ++) {
-	int n = (1UL << i);
-	for (int j = 0; j < 512; j += (2 * n))
-	  for (int k = 0; k < n; k ++) {
-		__m512i u = _mm512_and_si512(data[j + k], mask_l[i]);
-		__m512i v = _mm512_and_si512(data[j + k], mask_r[i]);
-		__m512i x = _mm512_and_si512(data[j + n + k], mask_l[i]);
-		__m512i y = _mm512_and_si512(data[j + n + k], mask_r[i]);
-		if (i <= 5) {
-		  data[j + k] = _mm512_or_si512(u, _mm512_srli_epi64(x, n));
-		  data[j + n + k] = _mm512_or_si512(_mm512_slli_epi64(v, n), y);
-		} else if (i == 6) {
-		  /* Note the "inversion" of srli and slli. */
-		  data[j + k] = _mm512_or_si512(u, _mm512_bslli_epi128(x, 8));
-		  data[j + n + k] = _mm512_or_si512(_mm512_bsrli_epi128(v, 8), y);
-		} else if (i == 7) {
-		  /* might be 0b01001110 instead */
-		  data[j + k] = _mm512_or_si512(u, _mm512_permutex_epi64(x,0b10110001));
-		  data[j + n + k] = _mm512_or_si512(_mm512_permutex_epi64(v,0b10110001), y);
-		} else {
-		  /* might be 0,1,2,3,4,5,6,7 */
-		  __m512i ctrl = _mm512_set_epi64(4,5,6,7,0,1,2,3);
-		  data[j + k] = _mm512_or_si512(u, _mm512_permutexvar_epi64(ctrl,x));
-		  data[j + n + k] = _mm512_or_si512(_mm512_permutexvar_epi64(ctrl,v), y);
-		}
-	  }
-  }
-}
 #else
 #error "no AVX?"
 #endif
+
 
 /* This code was generated by Usuba.
    See https://github.com/usubalang/usuba.
